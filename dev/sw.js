4c0ea8d644f9ebdae0af9c3da221abea59e08475
Buki4 <Buki4@users.noreply.github.com>
2026-06-17 07:19:34 +0000

    deploy: b8f2d1282815ce4f53c49568453ca00da36be314

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "workout-tracker-0.5.6";
+const CACHE = "workout-tracker-0.5.7";
 const APP_FILES = ['./', './index.html', './style.css', './app.js', './manifest.json'];
 
 // On install — cache core files and immediately activate (skipWaiting)
